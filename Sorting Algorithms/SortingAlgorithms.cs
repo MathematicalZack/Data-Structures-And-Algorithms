ed4d6d8325aa83e30a38bd05f2dd6003a2d24be7
Zack <therealzackroberts@gmail.com>
2018-10-14 15:46:14 +0300

    First Commit, add 'Sorting Algorithms' directory

---
+++ Sorting Algorithms/SortingAlgorithms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+static class SortingAlgorithms
+{
+
+    //Sorts list of generic type in quadratic time. Good for relatively short collections
+    //This implementation assumes an indexed parameter type
+    public static void InsertionSort<T>(IList<T> values) where T : IEquatable<T>, IComparable<T>, IComparable
+    {
+        for (int i = 1; i < values.Count; i++)
+        {
+            T key = values[i];
+            int j = i - 1;
+
+            while (j >= 0 && values[j].CompareTo(key) > 0)
+            {
+                values[j + 1] = values[j];
+                j--;
+            }
+
+            values[j] = key;
+        }
+    }
+
+}
+
